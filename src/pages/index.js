70c46e98d55f09992b8ef43cf40a20767d86e0e5
John Callaghan <john_callaghan@damibu.com>
2021-10-22 16:41:24 +0100

    More site structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
 import React from "react"
+import Layout from "../components/Layout"
+import * as  styles from '../styles/home.module.css'
 
 export default function Home() {
     return (
-        <h1>It's working</h1>
+        <Layout>
+            <section className={styles.hero}>
+                <p>The site is live.</p>
+                <img src="kiss1.jpg" alt="Chagall" style={{ maxWidth: '25%' }} />
+            </section>
+        </Layout>
     )
 }
