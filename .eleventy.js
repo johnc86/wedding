d89649e287c57a0813bc829caf4a2d1bfbfb50f9
johnc86 <johnc86@gmail.com>
2021-12-16 14:03:16 +0000

    Everything setup for docs folder
    better structure, new config, fixed js file urls

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,21 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     // Output directory: _site
     // Copy `img/` to `_site/img`
     eleventyConfig.addPassthroughCopy("assets/");
 
-	eleventyConfig.setBrowserSyncConfig({
-		files: [
+    eleventyConfig.setBrowserSyncConfig({
+        files: [
             './_site/css/**/*.css',
             './assets'
         ]
-	});
+    });
+};
+
+module.exports = function (eleventyConfig) {
+    return {
+        dir: {
+            input: "website",
+            output: "docs"
+        }
+    }
 };
