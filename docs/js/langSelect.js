10b6625be26fd66626292c75b140b2d1fa669375
johnc86 <johnc86@gmail.com>
2022-01-20 23:02:32 +0000

    Intro animation done, needs spinner to load images

--- docs/js/langSelect.js
+++ docs/js/langSelect.js
@@ -1,5 +1,6 @@
 var userLang = navigator.language || navigator.userLanguage; 
-    // alert ("The language is: " + userLang);
+// alert ("The language is: " + userLang);
+function detectLang() {
     if (userLang == "en-GB") {
         console.log("En detected")
         window.location = "/en/";
@@ -8,3 +9,5 @@ var userLang = navigator.language || navigator.userLanguage;
         console.log("PL detected")
         window.location = "/pl/";
     }
+}
+setTimeout(detectLang, 5000);
--- website/js/intro.js
+++ website/js/intro.js
@@ -1,21 +1,58 @@
-console.log("animation start");
-var paint = document.getElementsByClassName("paint");
 
-// The size of plottable area, the 500 is roughly the size of each asset to ensure the edges get covered
-var winh = window.innerHeight + 500;
-var winw = window.innerWidth + 500;
 
-for(var i = 0; i < paint.length; i++) {
-    // Randomise the width and height placement
-    var randh = Math.floor(Math.random() * winh);
-    var randw = Math.floor(Math.random() * winw);
 
-    // position x and y
-    paint[i].style.left = (randw - 250) + "px";
-    paint[i].style.top = (randh - 250) + "px";
+var introTime = 3000;
 
-    // Random rotation
-    var randr = Math.floor(Math.random() * 360);
-    paint[i].style.transform = "rotate(" + randr + ")";
+const timer = ms => new Promise(res => setTimeout(res, ms))
 
+async function addPaint () { // We need to wrap the loop into an async function for this to work
+    var numOfPaints = 250;
+    
+    // The size of plottable area, the 500 is roughly the size of each asset to ensure the edges get covered
+    var winh = window.innerHeight + 500;
+    var winw = window.innerWidth + 500;
+    
+    for(var i = 0; i < numOfPaints; i++) {
+        // add an image
+        var randimg = (Math.floor(Math.random() * 15)) + 1;
+        var randcol = (Math.floor(Math.random() * 4));
+        var paintcol;
+        if (randcol == 0) {
+            paintcol = "white";
+        }
+        else {
+            paintcol = "wedgblue"
+        }
+
+        var img = document.createElement("img");
+        img.src = "../img/" + paintcol + "/element" + randimg + ".png";
+        // img.src = "../img/" + paintcol + "/element" + 15 + ".png";
+        img.classList.add('paint');
+        var src = document.getElementById("cssintro");
+        src.appendChild(img);
+
+        // Randomise the width and height placement
+        var randh = Math.floor(Math.random() * winh);
+        var randw = Math.floor(Math.random() * winw);
+    
+        // position x and y
+        // paint[i].style.left = (randw - 250) + "px";
+        // paint[i].style.top = (randh - 250) + "px";
+        img.style.left = (randw - 250) + "px";
+        img.style.top = (randh - 250) + "px";
+    
+        // Random rotation
+        var randr = Math.floor(Math.random() * 360);
+        // paint[i].style.transform = "rotate(" + randr + ")";
+        img.style.transform = "rotate(" + randr + ")";
+
+        // lower opacity for white
+        if (paintcol == "white") {
+            img.style.opacity = "0.5"; 
+        }
+    
+        await timer(introTime/numOfPaints);
+    }
 }
+
+addPaint();
